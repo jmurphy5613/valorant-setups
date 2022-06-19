bf27b02bb031af0f1eb7046fe7d7317dd9d7b886
jmurphy5613 <jmurphy5613@gmail.com>
2022-06-19 05:11:06 -0400

    finished placeholder landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 
 import Icon from '../components/home/icon/Icon';
 
 const Home: NextPage = () => {
+
+  const router = useRouter();
+
   return (
     <div className={styles.root}>
       <Icon />
       <h1 className={styles["main-title"]}>Giving you the setups you need to rank up</h1>
-      <button className={styles.button}>Open App</button>
+      <button className={styles.button} onClick={e => router.push('/app')}>Open App</button>
     </div>
   )
 }
