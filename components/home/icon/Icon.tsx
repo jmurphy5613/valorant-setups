6396576e8386f41661e78c9c9cba75a53c08e854
jmurphy5613 <jmurphy5613@gmail.com>
2022-06-19 04:50:45 -0400

    created homepage with demo logo and colors

---
+++ components/home/icon/Icon.tsx
@@ -0,0 +1,10 @@
+import styles from './Icon.module.css';
+
+
+const Icon = () => {
+    return (
+        <img src="/logo.png" className={styles.icon} />
+    )
+}
+
+export default Icon
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+
+import Icon from '../components/home/icon/Icon';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-
+    <div className={styles.root}>
+      <Icon />
     </div>
   )
 }
