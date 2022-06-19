73b33a81f412e9885d69345f06b0f0a9b3602bcb
jmurphy5613 <jmurphy5613@gmail.com>
2022-06-19 05:01:05 -0400

    added title with gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ const Home: NextPage = () => {
   return (
     <div className={styles.root}>
       <Icon />
+      <h1 className={styles["main-title"]}>Giving you the setups you need to rank up</h1>
+      <button className={styles.button}>Open App</button>
     </div>
   )
 }
